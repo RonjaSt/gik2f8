f482ed550be4bc56a4313ea6a27a31123328327e
Lord_tensoon <89915147+RonjaSt@users.noreply.github.com>
2022-11-23 12:13:09 +0100

    whsgedkh
    shagdiuag

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 'use strict'
 
+console.log('test');
+
+const searchInput = null;
+const bookList = [
+    {
+    id: 1,
+    Author:'Brandon Sanderson',
+    title:'Mistborn'
+    },
+    {
+        id: 2,
+        Author:'Brent Weeks',
+        title:'The Way Of Shadows'
+        },
+];
+function handleKeyPress (input)  {
+    searchBooks(input);
+}
+function searchBooks(searchTerm){
+    
+    const filteredList= [];
+   for(let i = 0; i < bookList.length; i++){
+    const title= bookList[i].title.toLowerCase();
+    if(title.indexOf(searchTerm.toLowerCase())>= 0) {
+        filteredList.push(bookList[0]);
+
+    }  
+   }
+   renderBookList(filteredList);
+}
+searchBooks('o');
+function renderBookList(list){
+console.log(list);
+}
+
